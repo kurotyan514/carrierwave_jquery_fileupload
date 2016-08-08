1b6d4cc769becaf80e75f41d4a110b6950b328a2
kuro_sean <kurotyan514@gmail.com>
2016-08-08 12:02:52 +0800

    generate scaffold photo and do migrate

---
+++ app/views/photos/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @photo, :id, :product_id, :images, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808035810) do
+ActiveRecord::Schema.define(version: 20160808040138) do
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "product_id"
+    t.string   "images"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
