b21819026e7cf2635e80034761874599d3d2dcbc
kuro_sean <kurotyan514@gmail.com>
2016-08-09 12:27:31 +0800

    Gallery and Picture accsociation

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,31 +4,35 @@ class PicturesController < ApplicationController
   # GET /pictures
   # GET /pictures.json
   def index
-    @pictures = Picture.all
+    @gallery = Gallery.find(params[:gallery_id])
+    @pictures = @gallery.pictures
   end
 
   # GET /pictures/1
   # GET /pictures/1.json
   def show
+    @gallery = Gallery.find(params[:gallery_id])
   end
 
   # GET /pictures/new
   def new
-    @picture = Picture.new
+    @gallery = Gallery.find(params[:gallery_id])
+    @picture = @gallery.pictures.build
   end
 
   # GET /pictures/1/edit
   def edit
+    @gallery = Gallery.find(params[:gallery_id])
   end
 
   # POST /pictures
   # POST /pictures.json
   def create
-    @picture = Picture.new(picture_params)
-
+    @gallery = Gallery.find(params[:gallery_id])
+    @picture = @gallery.pictures.build(picture_params)
     respond_to do |format|
       if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
+        format.html { redirect_to gallery_picture_path(@gallery,@picture), notice: 'Picture was successfully created.' }
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new }
@@ -40,9 +44,10 @@ class PicturesController < ApplicationController
   # PATCH/PUT /pictures/1
   # PATCH/PUT /pictures/1.json
   def update
+    @gallery = Gallery.find(params[:gallery_id])
     respond_to do |format|
       if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
+        format.html { redirect_to gallery_picture_path(@gallery,@picture), notice: 'Picture was successfully updated.' }
         format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit }
@@ -54,9 +59,10 @@ class PicturesController < ApplicationController
   # DELETE /pictures/1
   # DELETE /pictures/1.json
   def destroy
+    @gallery = Gallery.find(params[:gallery_id])
     @picture.destroy
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
+      format.html { redirect_to gallery_pictures_path(@gallery), notice: 'Picture was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :pictures
-  resources :galleries
+root "galleries#index"
+  resources :galleries do
+    resources :pictures
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
